b45616c5f1a917455d8e0f7844cc9a48a5c9217f
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2017-12-30 02:33:19 +0000

    fixed spelling error

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function info() {
     alert("YARRRRRRRR!!!!!");
-    var natgeo = confirm("Yet's aren't real, so they aren't a danger to penguins. But climate change is. Click 'OK' to learn more about how climate change is threatening Penguins very existence.")
+    var natgeo = confirm("Yetis aren't real, so they aren't a danger to penguins. But climate change is. Click 'OK' to learn more about how climate change is threatening Penguins very existence.")
     open("https://news.nationalgeographic.com/2016/06/adelie-penguins-antarctica-climate-change-population-decline-refugia/");
 }
